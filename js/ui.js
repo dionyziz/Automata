7bddca3eefd772ced51411b726eb1557d72f2fd3
Dionysis Zindros <dionyziz@gmail.com>
2012-06-08 03:34:21 -0400

    Organize UI code

--- js/ui.js
+++ js/ui.js
@@ -1,115 +1,120 @@
-var canvas = document.getElementsByTagName( 'canvas' )[ 0 ];
-var ctx = canvas.getContext( '2d' );
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight - 39;
+var UI = {
+    canvas: null,
+    ctx: null,
+    inputString: '',
+    runStep: '',
+    displayRunDone: function() {
+        $( '.runtoolbar .step' ).hide();
+        $( '.runtoolbar .edit' ).show();
+    },
+    displayRejectedStatus: function() {
+        $( '#runstatus' ).html( 'Rejected' );
+        $( '#runstatus' ).addClass( 'reject icon' );
+        this.displayRunDone();
+    },
+    displayAcceptedStatus: function() {
+        $( '#runstatus' ).html( 'Accepted' );
+        $( '#runstatus' ).addClass( 'accept icon' );
+        this.displayRunDone();
+    },
+    displayRunStatus: function() {
+        $( '.runtoolbar code' ).html(
+              '<span class="seen">'
+            + this.inputString.substr( 0, this.runStep )
+            + '</span>'
+            + '<span class="current">'
+            + this.inputString.substr( this.runStep, 1 )
+            + '</span>'
+            + this.inputString.substr( this.runStep + 1 )
+        );
+        $( '.runtoolbar .step' ).show();
+        $( '.runtoolbar .edit' ).hide();
+        $( '#runstatus' ).html( 'Running' );
+    },
+    resize: function() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight - 39;
+    },
+    init: function() {
+        this.canvas = document.getElementsByTagName( 'canvas' )[ 0 ];
+        this.ctx = this.canvas.getContext( '2d' );
 
-window.onresize = resizeCanvas;
-document.onselectstart = function() {
-    return false;
-};
+        window.onresize = UI.resize.bind( this );
+        UI.resize();
+        document.onselectstart = function() {
+            return false;
+        };
 
-function resizeCanvas() {
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight - 39;
-};
+        $( '.toolbar .move' ).click( function() {
+            $( '.toolbar li' ).removeClass( 'selected' );
+            $( this ).addClass( 'selected' );
+            editor.setMode( 'moveState' );
+            return false;
+        } );
+        $( '.toolbar .transition' ).click( function() {
+            $( '.toolbar li' ).removeClass( 'selected' );
+            $( this ).addClass( 'selected' );
+            editor.setMode( 'createTransition' );
+            return false;
+        } );
+        $( '.toolbar .share' ).click( function() {
+            alert( 'Coming soon! :)' );
+            return false;
+        } );
 
-$( '.toolbar .move' ).click( function() {
-    $( '.toolbar li' ).removeClass( 'selected' );
-    $( this ).addClass( 'selected' );
-    editor.setMode( 'moveState' );
-    return false;
-} );
-$( '.toolbar .transition' ).click( function() {
-    $( '.toolbar li' ).removeClass( 'selected' );
-    $( this ).addClass( 'selected' );
-    editor.setMode( 'createTransition' );
-    return false;
-} );
-$( '.toolbar .share' ).click( function() {
-    alert( 'Coming soon! :)' );
-    return false;
-} );
+        this.runStep = 0;
+        var self = this;
 
-// TODO: Encapsulate these into an object
-var runStep = 0;
-var inputString;
+        $( '.toolbar .run' ).click( function() {
+            self.inputString = prompt( 'Enter input string: ', 'abba' );
+            $( '.runtoolbar' ).show();
+            $( '.toolbar' ).hide();
+            $( '#runstatus' ).removeClass( 'accept' );
+            $( '#runstatus' ).removeClass( 'reject' );
+            self.runStep = 0;
+            editor.run( self.inputString );
+            self.displayRunStatus();
+            return false;
+        } );
+        $( '.runtoolbar .step' ).click( function() {
+            if ( editor.runStep() ) {
+                ++self.runStep;
+                self.displayRunStatus();
+                if ( self.runStep == self.inputString.length ) {
+                    var accepting = false;
 
-function displayRunStatus() {
-    $( '.runtoolbar code' ).html(
-          '<span class="seen">'
-        + inputString.substr( 0, runStep )
-        + '</span>'
-        + '<span class="current">'
-        + inputString.substr( runStep, 1 )
-        + '</span>'
-        + inputString.substr( runStep + 1 )
-    );
-    $( '.runtoolbar .step' ).show();
-    $( '.runtoolbar .edit' ).hide();
-    $( '#runstatus' ).html( 'Running' );
-}
-function displayAcceptedStatus() {
-    $( '#runstatus' ).html( 'Accepted' );
-    $( '#runstatus' ).addClass( 'accept icon' );
-    displayRunDone();
-}
-function displayRejectedStatus() {
-    $( '#runstatus' ).html( 'Rejected' );
-    $( '#runstatus' ).addClass( 'reject icon' );
-    displayRunDone();
-}
-function displayRunDone() {
-    $( '.runtoolbar .step' ).hide();
-    $( '.runtoolbar .edit' ).show();
-}
-
-$( '.toolbar .run' ).click( function() {
-    inputString = prompt( 'Enter input string: ' );
-    $( '.runtoolbar' ).show();
-    $( '.toolbar' ).hide();
-    $( '#runstatus' ).removeClass( 'accept' );
-    $( '#runstatus' ).removeClass( 'reject' );
-    runStep = 0;
-    editor.run( inputString );
-    displayRunStatus();
-    return false;
-} );
-$( '.runtoolbar .step' ).click( function() {
-    if ( editor.runStep() ) {
-        ++runStep;
-        displayRunStatus();
-        if ( runStep == inputString.length ) {
-            var accepting = false;
-
-            for ( var state in nfaview.nfa.currentStates ) {
-                if ( nfaview.nfa.accept[ state ] ) {
-                    accepting = true;
-                    break;
+                    for ( var state in nfaview.nfa.currentStates ) {
+                        if ( nfaview.nfa.accept[ state ] ) {
+                            accepting = true;
+                            break;
+                        }
+                    }
+                    if ( accepting ) {
+                        self.displayAcceptedStatus();
+                    }
+                    else {
+                        self.displayRejectedStatus();
+                    }
+                }
+                else if ( nfaview.nfa.currentStates.length == 0 ) {
+                    self.displayRejectedStatus();
                 }
             }
-            if ( accepting ) {
-                displayAcceptedStatus();
-            }
-            else {
-                displayRejectedStatus();
-            }
-        }
-        else if ( nfaview.nfa.currentStates.length == 0 ) {
-            displayRejectedStatus();
-        }
-    }
-    return false;
-} );
-$( '.runtoolbar .edit' ).click( function() {
-    // run the final step
-    editor.runStep();
-    $( '.runtoolbar' ).hide();
-    $( '.toolbar' ).show();
-    return false;
-} );
+            return false;
+        } );
+        $( '.runtoolbar .edit' ).click( function() {
+            // run the final step
+            editor.runStep();
+            $( '.runtoolbar' ).hide();
+            $( '.toolbar' ).show();
+            return false;
+        } );
 
-var inputSymbol = document.getElementById( 'inputSymbol' );
-var errorSymbol = document.getElementById( 'errorSymbol' );
-var changeStateName = document.getElementById( 'changeStateName' );
-var editor = new NFAEditor( canvas, nfaview, [ inputSymbol, errorSymbol ], changeStateName );
-editor.play();
+        var inputSymbol = document.getElementById( 'inputSymbol' );
+        var errorSymbol = document.getElementById( 'errorSymbol' );
+        var changeStateName = document.getElementById( 'changeStateName' );
+        var editor = new NFAEditor( this.canvas, nfaview, [ inputSymbol, errorSymbol ], changeStateName );
+        editor.play();
+    }
+};
+UI.init();
