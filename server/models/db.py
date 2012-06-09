2bba9126bdcd671428aecf3d78655a7fe89ccad2
Kostis Karantias <karantiaskostis@gmail.com>
2012-06-09 07:28:58 -0400

    DB helper functions.

--- server/models/db.py
+++ server/models/db.py
@@ -8,6 +8,9 @@ class Database:
 
         self.conn = _mysql.connect( host = hostname, user = username, passwd = password, db = database )
 
+    def eq( value ):
+        return [ str( x ) + ' = \'' + str( value[ x ] ) + '\'' for x in value ]
+
     def close( self ):
         self.conn.close()
 
@@ -17,6 +20,15 @@ class Database:
 
     def array( self, sql ):
         return self.query( sql ).fetch_row( maxrows = 0, how = 1 )
+
+    def select( self, table, where = {}, select = [ '*' ] ):
+        if len( where ) == 0:
+            return self.array( 'SELECT %s FROM %s' % ( ','.join( select ), table ) )
+        return self.array( 'SELECT %s FROM %s WHERE %s' % ( ','.join( select ), table, ' AND '.join( self.eq( where ) ) ) )
+
+    def insert( self, table, value ):
+        self.query( 'INSERT INTO %s SET %s' % ( table, ','.join( self.eq( value ) ) ) )
+        return self.conn.insert_id()
 
 db = None
 
