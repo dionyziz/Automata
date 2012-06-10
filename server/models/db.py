a1725e5e0d9f11b42aa872d2879ccd8ab43a8dde
Kostis Karantias <karantiaskostis@gmail.com>
2012-06-10 16:28:31 -0400

    Rewritten db.py, using much safer executions, utf8.

--- server/models/db.py
+++ server/models/db.py
@@ -1,4 +1,4 @@
-import _mysql
+import MySQLdb
 
 class Database:
     def __init__( self, hostname, username, password, database ):
@@ -6,34 +6,52 @@ class Database:
         self.username = username
         self.database = database
 
-        self.conn = _mysql.connect( host = hostname, user = username, passwd = password, db = database )
-
-    def eq( self, value ):
-        return [ unicode( x ) + ' = \'' + unicode( value[ x ] ) + '\'' for x in value ]
+        self.conn = MySQLdb.connect(
+            host = hostname,
+            user = username,
+            passwd = password,
+            db = database,
+            use_unicode = True,
+            charset = 'UTF8' )
 
     def close( self ):
         self.conn.close()
 
-    def query( self, sql ):
-        self.conn.query( sql )
-        return self.conn.store_result()
+    def query( self, sql, data ):
+        cursor = self.conn.cursor()
+        cursor.execute( sql, data )
+        return cursor.fetchall()
 
-    def array( self, sql ):
-        return self.query( sql ).fetch_row( maxrows = 0, how = 1 )
-
-    def select( self, table, where = {}, select = [ '*' ] ):
+    def select( self, table, where = {}, select = ( '*' ) ):
+        """Runs a SELECT query in this form:
+            SELECT select FROM table
+            WHERE i in where < %s"""
+        sql = """SELECT %s FROM %s""" % ( ', '.join( select ), table )
         if len( where ) == 0:
-            return self.array( 'SELECT %s FROM %s' % ( ','.join( select ), table ) )
-        return self.array( 'SELECT %s FROM %s WHERE %s' % ( ','.join( select ), table, ' AND '.join( self.eq( where ) ) ) )
+            return self.query( sql, () )
 
-    def selectOne( self, table, where = {}, select = [ '*' ] ):
+        sql += """ WHERE"""
+        data = []
+        for w in where:
+            sql += """ """ + str( w ) + """ = %s"""
+            data.append( where[ w ] )
+        return self.query( sql, tuple( data ) )
+
+    def selectOne( self, table, where = {}, select = ( '*' ) ):
         rows = list( self.select( table, where, select ) )
         if len( rows ) == 0:
             return None
         return rows[ 0 ]
 
-    def insert( self, table, value ):
-        self.query( 'INSERT INTO %s SET %s' % ( table, ','.join( self.eq( value ) ) ) )
+    def insert( self, table, cols, values ):
+        """Runs an INSERT query of this form:
+            INSERT INTO table ( cols )
+            VALUES ( %s in values )"""
+        sql = """INSERT INTO %s ( %s ) VALUES ( %s )""" % ( table,
+            ', '.join( cols ),
+            ', '.join( [ '%s' for col in cols ] ) )
+        cursor = self.conn.cursor()
+        cursor.executemany( sql, values )
         return self.conn.insert_id()
 
 singletonDB = None
