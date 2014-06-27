7bbfcb6d40254ddd50d86cb133d5b735093d5148
kameranis <kameranis@gmail.com>
2014-06-27 14:55:22 +0300

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -219,28 +219,25 @@ NFAEditor.prototype = {
             this.runStep();
         }
     },
-    function customAlert(){
-    	this.render = function(dialog){
-		var winW = window.innerWidth;
-		var winH = window.innerHeight;
-		var dialogoverlay = document.getElementById('dialogoverlay');
-    		var dialogbox = document.getElementById('dialogbox');
-    		dialogoverlay.style.display = "block";
-    		dialogoverlay.style.height = winH+"px";
-    		dialogbox.style.width = "700px";
-    		dialogbox.style.left = (winW/2) - (600 * .5)+"px";
-    		dialogbox.style.top = "100px";
-    		dialogbox.style.display = "block";
-    		document.getElementById('dialogboxhead').innerHTML = "Keyboard Shortcut Help";
-    		document.getElementById('dialogboxbody').innerHTML = "<table style='width:500px'> <tr> <td>Double Click</td> <td>: Creates new State</td></tr><tr><td>Double Click on State</td><td>: Edits State</td></tr><tr><td>Left Click</td><td>: Selects State/Transition</td></tr><tr><td>Space</td><td>: Toggles Accepteness of current State</td></tr><tr><td>i</td><td>: Makes the current State initial</td></tr><tr><td>Delete</td><td>: Deletes current State/Transition and all assosiated Transitions</td></tr><tr><td>Esc</td><td>: Deselects State/Transition</td></tr><tr><td>?</td><td>: Shows Keyboard Help</td></tr></table>";
-    		document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
-	}
-    	this.ok = function(){
-    		document.getElementById('dialogbox').style.display = "none";
-    		document.getElementById('dialogoverlay').style.display = "none";
-    	}
+    render: function(){
+	var winW = window.innerWidth;
+	var winH = window.innerHeight;
+	var dialogoverlay = document.getElementById('dialogoverlay');
+    	var dialogbox = document.getElementById('dialogbox');
+    	dialogoverlay.style.display = "block";
+    	dialogoverlay.style.height = winH+"px";
+    	dialogbox.style.width = "600px";
+    	dialogbox.style.left = (winW/2) - (600 * .5)+"px";
+    	dialogbox.style.top = "100px";
+    	dialogbox.style.display = "block";
+    	document.getElementById('dialogboxhead').innerHTML = "Keyboard Shortcut Help";
+    	document.getElementById('dialogboxbody').innerHTML = "<table style='width:500px'><tr><td>Double Click</td><td>: Creates new State</td></tr><tr><td>Double Click on State</td><td>: Edits State</td></tr><tr><td>Left Click</td><td>: Selects State/Transition</td></tr><tr><td>Space</td><td>: Toggles Accepteness of current State</td></tr><tr><td>i</td><td>: Makes the current State initial</td></tr><tr><td>Delete</td><td>: Deletes current State/Transition and all assosiated Transitions</td></tr><tr><td>Esc</td><td>: Deselects State/Transition</td></tr><tr><td>?</td><td>: Shows Keyboard Help</td></tr></table>";
+    	document.getElementById('dialogboxfoot').innerHTML = '<button onclick="ok()">OK</button>';
     },
-    var Alert = new customAlert();
+    ok: function(){
+    	document.getElementById('dialogbox').style.display = "none";
+    	document.getElementById('dialogoverlay').style.display = "none";
+    },
     play: function() {
         // TODO: This function is huge and it includes many closures
         //       which long-term can cause garbage collection issues.
@@ -665,7 +662,7 @@ NFAEditor.prototype = {
                     self.inverseAccepting();
                     break;
                 case 63: // ?
-                    Alert.render();
+                    render();
                     break;
                 case 73: // i -- change initial state
                     if ( self.selectedElement[ 0 ] == 'state' ){
