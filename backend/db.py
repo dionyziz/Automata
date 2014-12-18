075eadfb88035a551593ffc570663f5d8d132104
Vasilis Spilka <vasspilka@gmail.com>
2014-12-18 10:38:48 +0200

    New structure

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 import MySQLdb
 import MySQLdb.cursors
-from sys import stderr
+import logging
 
 
 class Database:
@@ -27,7 +27,7 @@ class Database:
         self.conn.close()
 
     def query(self, sql, data):
-        stderr.write("Executing SQL query: %s\n" % (sql))
+        logging.debug("Executing SQL query: %s\n" % (sql))
 
         cursor = self.conn.cursor()
         try:
@@ -43,20 +43,18 @@ class Database:
         return cursor.fetchall()
 
     def select(self, table, where={}, select=('*')):
-        """Runs a SELECT query in this form:
-            SELECT select FROM table
-            WHERE i in where < %s"""
-        sql = """SELECT %s FROM %s""" % (', '.join(select), table)
+        """SELECT (select) FROM (table) WHERE (where)"""
+        sql = "SELECT %s FROM %s" % (', '.join(select), table)
         if len(where) == 0:
             return self.query(sql, ())
-
-        sql += """ WHERE"""
+        sql += " WHERE"
         data = []
         for w in where:
-            sql += """ """ + str(w) + """ = %s"""
+            sql += " " + str(w) + " = %s"
             data.append(where[w])
         return self.query(sql, tuple(data))
 
+    # Runs a SELECT but return only first row
     def selectOne(self, table, where={}, select=('*')):
         rows = self.select(table, where, select)
         if len(rows) == 0:
@@ -64,13 +62,11 @@ class Database:
         return rows[0]
 
     def insert(self, table, data):
-        """Runs an INSERT query of this form:
-            INSERT INTO table ( cols )
-            VALUES ( %s in values )"""
+        """INSERT (data) INTO (table)"""
         cols = tuple(data.keys())
         values = tuple(data.values())
-        sql = """INSERT INTO %s ( %s ) VALUES ( %s )""" % (table, ', '.join(cols),
-                                                           ', '.join(['%s' for col in cols]))
+        sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (
+              table, ', '.join(cols), ', '.join(['%s' for col in cols]))
         self.query(sql, values)
         return self.conn.insert_id()
 
